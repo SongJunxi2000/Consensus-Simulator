5b1a0fcc0dd5cb5503a2ab424a24f98c67c6c9da
Junxi Song <junxisong2000@gmail.com>
2020-10-13 20:59:19 -0400

    Update

--- Simulator/src/Player.java
+++ Simulator/src/Player.java
@@ -6,17 +6,28 @@ class Message{
     int sender;
     boolean readyForDelivery;
     int sendRound;
+    long sig;
 };
-public abstract class Player {
-    private int player_id;
-    public Player(int ID){
-        player_id = ID;
+public class Player {
+    private int player_private_key;
+    public int player_id;
+    public Player(int key, int id){
+        player_private_key = key;
+        player_id = id;
+
     }
-    public void send(Message msg){ Simulation_engine.Fauth.send(msg.sender = player_id);
+    public void send(Message msg){
+        Fauth.send(msg.sender = player_id, player_private_key);
     }
     public LinkedList<Message> receive(){
-        return Simulation_engine.Fauth.receive(player_id);
+
+        return Simulation_engine.Fauth.receive(player_private_key, player_id);
     }
-    public void endRound(){Simulation_engine.endRound(player_id);}
-    public void terminate(){Simulation_engine.terminate(player_id);}
+    public void endRound(){
+
+        Simulation_engine.endRound(player_private_key);
+    }
+    public void terminate(){
+        Simulation_engine.terminate(player_private_key);
+    }
 }
--- Simulator/src/Simulation_engine.java
+++ Simulator/src/Simulation_engine.java
@@ -1,30 +1,84 @@
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Random;
 
 public class Simulation_engine {
     public int numOfPlyaers, numOfFaultyPlayers, delay, maxRound;
     public int roundNumber=0;
-    private Player[] players;
+    private HashMap<Integer, Player> players;
+    public int[] players_key;
+
+    Random rand = new Random();
 
     public Simulation_engine(int numOfPlayers, int numOfFaultyPlayers, int delay, int maxRound){
         this.numOfFaultyPlayers = numOfFaultyPlayers;
         this.numOfPlyaers = numOfPlayers;
         this.delay = delay;
         this.maxRound = maxRound;
-        players = new Player[numOfPlayers];
         for(int i=0;i<numOfPlayers;i++){
             //initialize the player, random int + hashmap? or use the sequence of id
-        }
-    }
-
-    public class Fauth{
-        LinkedList<Message> messages;// maybe need an array? 
-        public Fauth(){
-            messages = new LinkedList<Message>();
-        };
-        public void send(Message msg){
-            msg.sendRound = roundNumber;
-            msg.readyForDelivery = Adversary.deliverPermission(msg);
-            if
+            int key = rand.nextInt();
+            players.put(key, new Player(key, i));
+            players_key[i] = key;
         }
     }
 }
+
+    public class Fauth{
+        HashMap<Integer, LinkedList<Message>> ready_messages;// key is the receiver
+        LinkedList<Message> unready_message;
+
+        private void checkReady(Message msg){
+            msg.readyForDelivery = Adversary.deliverPermission(msg);
+            if(msg.readyForDelivery){
+                ready_messages.get(msg.sender).add(msg);
+            }
+            else{
+                unready_message.add(msg);
+            }
+        }
+        public void send(Message msg, int player_key){
+            if(players_key[msg.sender]!= player_key) return;
+            msg.sendRound = roundNumber;
+            msg = Fsign.sign(msg);
+            checkReady(msg);
+        }
+
+        public LinkedList<Message> receive(int key, int id){
+            if(players_key[id]!= key) return null;
+            return ready_messages.get(key);
+        }
+
+        public void update(){
+            ListIterator<Message> iterator = unready_message.listIterator();
+            while(iterator.hasNext()){
+                Message msg = iterator.next();
+                msg.readyForDelivery = Adversary.deliverPermission(msg);
+                if(msg.readyForDelivery) {
+                    ready_messages.get(msg.sender).add(msg);
+                    unready_message.remove(msg);
+                }
+            }
+        }
+
+        private class Fsign{
+            private HashMap<Long, Message> signed_messages;
+            protected Message sign(Message msg){
+                long signature = rand.nextLong();
+                while(signed_messages.containsKey(signature)) {
+                    signature = rand.nextLong();
+                }
+                msg.sig = signature;
+                signed_messages.put(signature,msg);
+                return msg;
+            }
+
+            public boolean check_sig(Message msg, Long sig){
+                return signed_messages.containsKey(sig);
+            }
+        }
+    }
+
+
+
