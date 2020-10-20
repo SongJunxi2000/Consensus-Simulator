58a6bd472865d03f11b6a3121384d7adbfb695f0
Junxi Song <junxisong2000@gmail.com>
2020-10-19 22:12:54 -0400

    Fix bugs

--- Simulator/src/Adversary.java
+++ Simulator/src/Adversary.java
@@ -1,29 +1,31 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Adversary {
-    HashMap<Integer, LinkedList<Message>> ready_messages;
+    HashMap<Integer, LinkedList<Message>> ready_messages,temp;
     LinkedList<Message> unready_message;
-    List<Player> faulty_players;
+    LinkedList<Player> faulty_players;
     int numOfPlayers;
     int numOfFaultyPlayers;
     int delay;
     int maxRound;
-    public Adversary(List<Player> faulty_players, int numOfPlayers, int numOfFaultyPlayers, int delay, int maxRound){
+    public Adversary(LinkedList<Player> faulty_players, int numOfPlayers, int numOfFaultyPlayers, int delay, int maxRound){
         this.faulty_players = faulty_players;
         this.numOfPlayers = numOfPlayers;
         this.numOfFaultyPlayers = numOfFaultyPlayers;
         this.delay = delay;
         this.maxRound = maxRound;
     }
-    public sendInThisRound(){
-        attack()
-        HashMap<Integer, LinkedList<Message>> temp = new HashMap<Integer, LinkedList<Message>>();
-        temp = ready_messages.clone();
+    public HashMap<Integer, LinkedList<Message>> sendInThisRound(){
+        attack();
+        temp = (HashMap<Integer, LinkedList<Message>>)ready_messages.clone();
         ready_messages = new HashMap<Integer, LinkedList<Message>>();
         return temp;
     }
-    public receive(Message message){
+    public void receive(Message message){
         unready_message.add(message);
     }
-    public attack(){
+    public void attack(){
 
     }
 }
