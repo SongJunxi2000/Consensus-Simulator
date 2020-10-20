a97d1499c06cad1b88079bcad21e0f9b036efc62
Junxi Song <junxisong2000@gmail.com>
2020-10-19 21:39:04 -0400

    Change the signature to a local action, and finished signature

--- Simulator/src/Player.java
+++ Simulator/src/Player.java
@@ -3,13 +3,24 @@ import java.util.LinkedList;
 public class Player {
     private int player_private_key;
     public int player_id;
-    public Player(int key, int id){
+    public int round_number;
+    public Fauth auth;
+    public Fsign sign;
+
+    public Player(int key, int id, Fauth authenticate, Fsign signature){
         player_private_key = key;
         player_id = id;
-
+        auth = authenticate;
+        sign = signature;
     }
-    public void send(Message msg){
-        Fauth.send(msg.sender = player_id, player_private_key);
+    public void update_round(int RN, int private_key){
+        if(private_key == player_private_key) {
+            round_number = RN;
+        }
+    }
+    public void send(String msg, int receiver){
+        Message message = sign.sign(msg, player_id, player_private_key,round_number,player_private_key);
+        auth.send(message);
     }
     public LinkedList<Message> receive(){
 
