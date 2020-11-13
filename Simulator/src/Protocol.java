084f3e3b71a527a901bf00caf4c20cab64931ad6
Junxi Song <junxisong2000@gmail.com>
2020-11-12 21:29:26 -0500

    update potoco interface

--- Simulator/src/Protocol.java
+++ Simulator/src/Protocol.java
@@ -3,5 +3,5 @@ import java.util.LinkedList;
 public interface Protocol {
     public String input(int id, int round_number);
     public void action(Player player);
-    public boolean check_output(int designated_sender, LinkedList<Player> honest_players, int[] outputs);
+    public boolean check_output(int designated_sender, LinkedList<Integer> honest_players_id, int[] outputs);
 }
