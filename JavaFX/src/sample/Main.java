43a6bbb6a881319e69acf34c9ddd2076f5254382
Kai Zou <zkjzou@outlook.com>
2021-01-08 16:40:54 -0500

    more GUI

--- JavaFX/src/sample/Main.java
+++ JavaFX/src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Simulator.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
     }
 
