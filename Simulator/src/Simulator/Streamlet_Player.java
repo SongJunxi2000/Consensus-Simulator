efd38ec9151dae334031e464cc998bed4960c438
Junxi Song <junxisong2000@gmail.com>
2021-01-23 17:00:19 -0500

    stremlet player untested

--- Simulator/src/Simulator/Streamlet_Player.java
+++ Simulator/src/Simulator/Streamlet_Player.java
@@ -5,26 +5,34 @@ import com.google.gson.Gson;
 import java.util.LinkedList;
 
 public class Streamlet_Player extends Player {
-    public Streamlet_Player(int key, int id, Fauth authenticate, Fsign signature, Simulation_engine engine, int num, boolean isSender) {
-        super(key, id, authenticate, signature, engine, num);
-    }
-
     int epochN;
     BlockTree log;
     Gson gson = new Gson();
-    Block proposed;
+    StremletM proposed;
+    Block blockAfter;
+
+
+    public Streamlet_Player(int key, int id, Fauth authenticate, Fsign signature, Simulation_engine engine, int num, boolean isSender) {
+        super(key, id, authenticate, signature, engine, num);
+    }
 
     //based on the current longest notarized, the player decide whether to vote on the proposed block.
     // If the propose block is legal, the player signs the block and send it to everyone
     public void vote(){
         LinkedList<Message> voteBlocks = receive();
         for(int i=0;i<voteBlocks.size();i++){
+            //cBlock = (msg,receiver,sender,sendround)
             Message cBlock = voteBlocks.get(i);
             if(cBlock.getSender()== engine.designated_sender && sign.verification(cBlock.getMsg())){
-                proposed = new Block(cBlock.getMsg());
+                //signM = ((h,e,txs),p,sig)
+                signedM signM = gson.fromJson(cBlock.getMsg(),signedM.class);
+                //block = (h,e,txs)
+                String block = signM.msg;
+                proposed = gson.fromJson(block,StremletM.class);
                 LinkedList<Block> longest = log.getLongest();
                 for(int j=0;j<longest.size();j++){
-                    int curHash = longest.get(j).m.hashCode();
+                    blockAfter = longest.get(i);
+                    int curHash = blockAfter.m.hashCode();
                     if(curHash == Integer.valueOf(proposed.h)){
                         for(int k = 0;k<total_num_of_players;k++){
                             send(sign(cBlock.getMsg()),i,engine.roundNumber);
@@ -36,18 +44,26 @@ public class Streamlet_Player extends Player {
             }
 
         }
-
-
-
     }
     //if the proposed block receives more than 2n/3 votes, add it to the longest notarized chain
     public void notraize(){
         LinkedList<Message> votes = receive();
-
-
-    }
-    //if the longest noatarized chain has 3 consective blocks, finalize in the block
-    public void stream_finalize(){
+        int[] countVotes = new int[total_num_of_players];
+        int count = 0;
+        for(int i=0;i<votes.size();i++){
+            Message vote = votes.get(i);
+            if(countVotes[vote.getSender()]==0 && sign.verification(vote.getMsg())){
+                signedM signM = gson.fromJson(vote.getMsg(),signedM.class);
+                if(gson.toJson(proposed).equals(signM.msg))
+                {
+                    countVotes[vote.getSender()]=1;
+                    count++;
+                }
+            }
+        }
+        if(count*2/3>total_num_of_players){
+            log.add(gson.toJson(proposed),blockAfter);
+        }
 
     }
     // choose the longest notarized chain, and propose the block after it.
@@ -60,6 +76,24 @@ public class Streamlet_Player extends Player {
         for(int i = 0;i<total_num_of_players;i++){
             send(sign(msg),i,e);
         }
+    }
+
+    public void action(String txs){
+        if(txs != null){
+            propose(txs);
+        }
+        vote();
+        notraize();
+    }
+}
+class StremletM{
+    String txs;
+    String h;
+    int e;
+    public StremletM(String h, int e, String txs){
+        this.h = h;
+        this.e = e;
+        this.txs = txs;
     }
 }
 
